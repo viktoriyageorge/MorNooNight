4d38bd81f588434b729a4151b387befed4acbaec
viktoriyageorge <viktoriyageorge@gmail.com>
2024-03-03 14:53:22 +0200

    add the new frontend version

--- HappyNotes/src/main/java/bg/fmi/HappyNotes/configuration/WebConfig.java
+++ HappyNotes/src/main/java/bg/fmi/HappyNotes/configuration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("https://happy-notes-95ytmumzm-viktoriyageorges-projects.vercel.app",
-                        "https://happy-notes-rg1ownbp4-viktoriyageorges-projects.vercel.app")
+                        "https://happy-notes-qi0ksmusn-viktoriyageorges-projects.vercel.app")
                 .allowedMethods("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("Content-Type")
                 .allowCredentials(true);
--- HappyNotes/src/main/java/bg/fmi/HappyNotes/configuration/WebSocketConfig.java
+++ HappyNotes/src/main/java/bg/fmi/HappyNotes/configuration/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setAllowedOrigins("http://localhost:8102",
                         "http://192.168.0.103:8102",
                         "https://happy-notes-95ytmumzm-viktoriyageorges-projects.vercel.app",
-                        "https://happy-notes-rg1ownbp4-viktoriyageorges-projects.vercel.app")
+                        "https://happy-notes-qi0ksmusn-viktoriyageorges-projects.vercel.app")
                 .withSockJS();
     }
 
